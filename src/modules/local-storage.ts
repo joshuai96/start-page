03fb76d7742fb84202629cccb37548eb595bf7e8
Joshua Irmer <joshua.irmer@gmail.com>
2025-03-23 00:11:34 +0100

    feat: add local storage

---
+++ src/modules/local-storage.ts
@@ -0,0 +1,33 @@
+import { IAppData } from "../interfaces/appdata"
+
+class LocalStorage {
+    storageKey = 'app-data';
+
+    save(data: IAppData) {
+        const rawData = JSON.stringify(data, undefined, 0);
+
+        console.log(rawData);
+
+        localStorage.setItem(this.storageKey, rawData);
+    }
+
+    load(): IAppData {
+        const rawData = localStorage.getItem(this.storageKey)
+
+        if (!rawData) {
+            return {
+                meta: {
+                    title: "start-page",
+                    version: 1
+                },
+                data: {
+                    groups: []
+                }
+            }
+        }
+
+        return JSON.parse(rawData) as IAppData
+    }
+}
+
+export default LocalStorage
