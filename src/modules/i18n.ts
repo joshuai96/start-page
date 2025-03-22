6884ab4ec3c0e24f23cf12977c56739fdecc53a9
Joshua Irmer <joshua.irmer@gmail.com>
2025-03-23 00:11:22 +0100

    feat: add translations

---
+++ src/modules/i18n.ts
@@ -0,0 +1,26 @@
+import i18n, {InitOptions} from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from '../lang/en.json'
+import de from '../lang/de.json'
+
+export function init() {
+    const initOptions: InitOptions = {
+        resources: {
+            en: {
+                translation: en
+            },
+            de: {
+                translation: de
+            }
+        },
+        lng: "de",
+        fallbackLng: "en",
+        saveMissing: true
+    };
+    i18n
+        .use(initReactI18next)
+        .init(initOptions)
+        .catch((err: unknown) => {
+            console.warn(err);
+        });
+}
